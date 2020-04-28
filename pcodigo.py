408cbbed3fd3e93ba5c981c51dd6e26284389a08
Marvin <lopez.marvin9@gmail.com>
2020-04-27 23:54:25 -0600

    Cambios finales

--- pcodigo.py
+++ pcodigo.py
@@ -1,4 +1,5 @@
 import gi
+import predecir
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf
 
@@ -8,14 +9,23 @@ class Handler:
         Gtk.main_quit()
     
     def directory_set(self, button):
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(button.get_filename(),250,250,preserve_aspect_ratio=True)
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(button.get_filename(),500,500,preserve_aspect_ratio=True)
         image.set_from_pixbuf(pixbuf)
-        # print(button.get_filename())
-    
-    #metodo para agregar la descipcion
-    def description_set(self, button):
+        print(button.get_filename())
+        respuesta= predecir.predict(button.get_filename())
         textbuffer = text.get_buffer()
-        textbuffer.set_text("Hola mundo!")
+        if respuesta==0:
+            textbuffer.set_text("FASE 1\nPROPAGACION DE PLANDULAS\nEdad: 2-3 semanas de vida\nTiempo de cosecha: 13-14 semanas aprox.\nINFO: la propagacion de plnadulas consiste en el desarrollo\ninicial de la planta, desde la germinación hasta obtener las primeras ramas principales.")
+        elif respuesta==1:
+            textbuffer.set_text("FASE 2:\n DESARROLLO DE RAICES\nEdad: 4-6 semanas de vida\nTiempo de cosecha: 10-12 semanas aprox.\nINFO: Se recomienda transplantar a un area mas grande para que\nla planta pueda enraizar correctamente.")
+        elif respuesta==2:
+            textbuffer.set_text("FASE 6:\nCOSECHA\nEdad: 16- semanas de vida\nTiempo de cosecha: AHORA\nINFO: Coseche según sea su necesidad, Para obtener un sabor\nunico espere hasta que este completamente rojo.")
+        elif respuesta==3: 
+            textbuffer.set_text("FASE 3:\nMADURACION O DE CRECIMIENTO\nEdad: 7-11 semanas de vida\nTiempo de cosecha: 5-11 semanas aprox.\nINFO: Produre agregar un soporte para que la planta crezca\n correctamente. Se recomienda abonar a cada 10-15 dias para\n que la planta crezca correctamente.")
+        elif respuesta==4:
+            textbuffer.set_text("FASE 4:\nFLORACION\nEdad: 12-13 semanas de vida\nTiempo de cosecha: 3-4 semanas aprox.\nINFO: Las flores amarillas son los puntos en donde creceran\nlos frutos. Los primeros brotarán en la parte baja de la planta.")
+        elif respuesta==5:
+            textbuffer.set_text("FASE 5:\nPRODUCCION\nEdad: 14-15 semanas de vida\nTiempo de cosecha: 1-2 semanas aprox.\nINFO: La planta ha iniciado a desarrollar sus frutos.\nDebe esperar a que crezcan y muestren un color rojizo-anaranjado.")
 
 #constructor del archivo glade
 builder = Gtk.Builder()
